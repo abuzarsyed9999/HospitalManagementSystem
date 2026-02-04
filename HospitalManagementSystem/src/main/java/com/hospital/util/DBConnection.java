a34ceeb64bcef74c70146e129f8ed4f9f22ec878
abuzarsyed9999 <abuzarcda@gmail.com>
2026-02-04 23:03:47 +0530

    Update DBConnection.java

--- HospitalManagementSystem/src/main/java/com/hospital/util/DBConnection.java
+++ HospitalManagementSystem/src/main/java/com/hospital/util/DBConnection.java
@@ -8,7 +8,7 @@ public class DBConnection {
     // Database credentials
     private static final String URL = "jdbc:mysql://localhost:3306/hospital_db?useSSL=false&serverTimezone=UTC";
     private static final String USERNAME = "root";  
-    private static final String PASSWORD = "#Abuzar99"; 
+    private static final String PASSWORD = "password"; 
 
      
     public static Connection getConnection() {
